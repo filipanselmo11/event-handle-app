7b543bd642ca872ba4ad3debfd85ade75292a6d3
Fílip Anselmo <filip.anselmo@jointecnologia.com.br>
2022-07-06 15:22:30 -0400

    handling-events

---
+++ src/components/DialogComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DialogComponent",
+  data() {
+    return {
+      time: undefined,
+    };
+  },
+  methods: {
+    onClose() {
+      this.time = Date.now();
+      this.$emit("dialog-closed", this.time);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <dialog-component @dialog-closed="eventHandler"></dialog-component>
+  </div>
+</template>
+
+<script>
+import DialogComponent from "./DialogComponent.vue";
+
+export default {
+  components: { DialogComponent },
+  name: "MainComponent",
+  methods: {
+    eventHandler(e, time) {
+      console.log(`Dialog was closed at ${time}`);
+    },
+  },
+};
+</script>
+
+<style></style>
